6d26f5469008407e9a788fd1846c4bc635ce0391
magmafellow <138253744+magmafellow@users.noreply.github.com>
2026-03-05 00:03:24 +0300

    middleware added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,29 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostModule } from './post/post.module';
 import { CatsController } from './cats.controller';
+import { LoggerMiddleware } from './logger.middleware';
 
 @Module({
   imports: [PostModule],
   controllers: [AppController, CatsController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware)
+      .exclude({ path: 'cats/anroe', method: RequestMethod.ALL })
+      .forRoutes(
+        { path: 'cats', method: RequestMethod.ALL },
+        { path: 'post', method: RequestMethod.POST },
+        'cats/{*splat}',
+      );
+  }
+}
